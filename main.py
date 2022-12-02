fc10fd7296eb9f8a85eb480a268f8b77d48146ec
Bridget Sitkoff <bridget_sitkoff@milton.edu>
2022-12-02 17:02:14 -0500

    Added a runner - need to clean up data input.

---
+++ main.py
@@ -0,0 +1,3 @@
+# Repl.it AOC Python Runner
+import runner
+runner.run(day=2)
---
+++ template.py
@@ -0,0 +1,13 @@
+
+
+def part1(data):
+  __import__('time').sleep(1)
+  # Data is automatically read from 01.txt
+  data_list = data.split()
+  return(0)
+
+def part2(data):
+  __import__('time').sleep(1)
+  # Data is automatically read from 01.txt
+  data_list = data.split()
+  return(0)
