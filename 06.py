d0cdf11fd475eec36a62606a50125c0db77b2a65
Bridget Sitkoff <bridget_sitkoff@milton.edu>
2022-12-06 21:27:50 -0500

    Day five with excessive assistnace.

--- 06.py
+++ 06.py
@@ -1,3 +1,10 @@
 from aocd import lines  # like data.splitlines()
 import numpy
 print(lines)
+data = lines.split()
+alpha = {}
+for val in data:
+    if val in alpha:
+        print(f'Saw {val} before')
+    else:
+        print(f'{val} is new/')
